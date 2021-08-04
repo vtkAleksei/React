1dbca7c687a0a63db0814e6c086c122d0461cdae
vtkAleksei <vtkaleksei@yandex.ru>
2021-08-04 22:56:24 +0400

    lesson 4

---
+++ myapp/src/Components/Form.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Components_style/Form.css';
+import { useState, useRef, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+export const Form = ({ onSendMessage }) => {
+    const [value, setValue] = useState('');
+    const inputRef = useRef(null);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log("handleSubmit----");
+
+        onSendMessage({
+            author: 'Me',
+            text: value,
+            id: Date.now(),
+        });
+
+        setValue('');
+        inputRef.current.focus();
+    }
+
+    useEffect (()=>{
+        inputRef.current.focus();
+    },[]);
+
+
+    return (
+        <form>
+            <TextField inputRef={inputRef} id="outlined-basic" label="Сообщение" variant="outlined" color="secondary" value={value} onChange={handleChange} />
+            <Button variant="outlined" color="secondary" onClick={handleSubmit} >Отправить</Button>
+        </form>
+    )
+
+}
---
+++ myapp/src/Components/Profile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (<h1>Welcome to the page Profile</h1>)
+}
